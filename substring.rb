966ced2b728d8ec8106c628d747738a3fd3cc89f
Jose Naylor <jose.c.naylor@gmail.com>
2017-03-22 15:41:04 +0000

    substring method finished can now make a hash of listing sub string in sentence and how many times it was found

--- substring.rb
+++ substring.rb
@@ -1,2 +1,20 @@
-def substring(sentence,dictionary)
+def substrings(sentence, dictionary)
+  listing_hash = {}
+  sentence_array = sentence.downcase.split(" ")
+  dictionary.each do |d|
+    counter = 0
+    sentence_array.each do |w|
+      if w.include? d 
+        counter += 1
+      end
+    end
+    if counter > 0
+      listing_hash[d] = counter
+    end
+  end
+  puts listing_hash
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
